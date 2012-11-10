7af44c6e0ac35d8a28db155b0254e70dd5c8a360
TheCellarRoom <TheCellarRoom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-10 12:13:41 +0000

    Reclaimed ownership, added banner image, rolled on version number, corrected screenshot.
    git-svn-id: https://plugins.svn.wordpress.org/quick-review-post-access/tags/1.3.0@623341 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Quick_Review_Post_Access.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * @package Quick Review Post Access
+ * @author The Cellar Room Limited
+ * @version 1.3.0
+ */
+/*
+Plugin Name: Quick Review Post Access
+Version: 1.3.0
+Plugin URI: http://thecellarroom.co.uk
+Author: The Cellar Room Limited
+Author URI: http://www.thecellarroom.co.uk
+Description: Adds a link to 'Pending', 'future' (scheduled) and 'Drafts' under the Posts, Pages, and other custom post type sections in the admin menu. Compatible with WordPress 3.0+.
+
+*/
+
+/*
+Copyright (c) 2010 by The Cellar Room Limited
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
+modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+if ( is_admin() && !class_exists( 'TCR_QuickDraftsAccess' ) ) :
+
+class TCR_QuickDraftsAccess {
+
+	function init() {
+		add_action( 'admin_menu', array( __CLASS__, 'quick_drafts_access' ) );
+	}
+
+	function quick_drafts_access() {
+		$post_types = (array) get_post_types( array( 'show_ui' => true ), 'object' );
+		$post_types = apply_filters( 'TCR_quick_drafts_access_post_types', $post_types );
+		foreach ( $post_types as $post_type ) {
+			$name = $post_type->name;
+			$num_posts = wp_count_posts( $name, 'readable' );
+			$path = 'edit.php';
+			if ( 'post' != $name ) // edit.php?post_type=post doesn't work
+				$path .= '?post_type=' . $name;
+			if ( ( $num_posts->draft > 0 ) || apply_filters( 'TCR_quick_drafts_access_show_if_empty', false, $name, $post_type ) )
+				add_submenu_page( $path, __( 'Drafts' ), sprintf( __( 'Drafts (%d)' ), $num_posts->draft ), $post_type->cap->edit_posts, "edit.php?post_type=$name&post_status=draft" );
+		}
+	}
+
+}
+
+TCR_QuickDraftsAccess::init();
+
+endif;
+?>
+<?php
+if ( is_admin() && !class_exists( 'TCR_QuickPendingAccess' ) ) :
+
+class TCR_QuickPendingAccess {
+
+	function init() {
+		add_action( 'admin_menu', array( __CLASS__, 'quick_Pending_access' ) );
+	}
+
+	function quick_Pending_access() {
+		$post_types = (array) get_post_types( array( 'show_ui' => true ), 'object' );
+		$post_types = apply_filters( 'TCR_quick_Pending_access_post_types', $post_types );
+		foreach ( $post_types as $post_type ) {
+			$name = $post_type->name;
+			$num_posts = wp_count_posts( $name, 'readable' );
+			$path = 'edit.php';
+			if ( 'post' != $name ) // edit.php?post_type=post doesn't work
+				$path .= '?post_type=' . $name;
+			if ( ( $num_posts->pending > 0 ) || apply_filters( 'TCR_quick_Pending_access_show_if_empty', false, $name, $post_type ) )
+				add_submenu_page( $path, __( 'Pending' ), sprintf( __( 'Pending (%d)' ), $num_posts->pending ), $post_type->cap->edit_posts, "edit.php?post_type=$name&post_status=pending" );
+		}
+	}
+}
+
+TCR_QuickPendingAccess::init();
+endif;
+?>
+<?php
+if ( is_admin() && !class_exists( 'TCR_QuickfutureAccess' ) ) :
+
+class TCR_QuickfutureAccess {
+
+	function init() {
+		add_action( 'admin_menu', array( __CLASS__, 'quick_future_access' ) );
+	}
+
+	function quick_future_access() {
+		$post_types = (array) get_post_types( array( 'show_ui' => true ), 'object' );
+		$post_types = apply_filters( 'TCR_quick_future_access_post_types', $post_types );
+		foreach ( $post_types as $post_type ) {
+			$name = $post_type->name;
+			$num_posts = wp_count_posts( $name, 'readable' );
+			$path = 'edit.php';
+			if ( 'post' != $name ) // edit.php?post_type=post doesn't work
+				$path .= '?post_type=' . $name;
+			if ( ( $num_posts->future > 0 ) || apply_filters( 'TCR_quick_future_access_show_if_empty', false, $name, $post_type ) )
+				add_submenu_page( $path, __( 'future' ), sprintf( __( 'future (%d)' ), $num_posts->future ), $post_type->cap->edit_posts, "edit.php?post_type=$name&post_status=future" );
+		}
+	}
+}
+
+TCR_QuickfutureAccess::init();
+endif;
+?>
---
+++ ReadMe.txt
@@ -0,0 +1,41 @@
+=== Plugin Name ===
+Contributors: TCR (http://www.thecellarroom.net)
+Donate link: http://www.thecellarroom.co.uk
+Tags: Pending, future, scheduled, Drafts, Posts, Pages, access, quick, review
+Requires at least: 2.0.2
+Tested up to: 1.0.1
+Stable tag: 1.3.0
+
+Adds a link to 'Pending', 'future' (scheduled) and 'Drafts' under the Posts
+
+== Description ==
+
+Adds a link to 'Pending', 'future' (scheduled) and 'Drafts' under the Posts, Pages, and other custom post type sections in the admin side menu. Compatible with WordPress 3.0+.
+
+== Installation ==
+
+1. Upload the .php file to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+ask one?
+
+== Screenshots ==
+
+1. screenshot-1.png 
+
+== Changelog ==
+
+= 1.3.0 =
+* fixed screenshot, added header image
+
+= 1.0.1 =
+* Added readme.txt file, screenshots etc
+
+= 1.0 =
+* First submission to Wordpress
+
+== Upgrade Notice ==
+
+none to speak of yet, get in touch if you have a feature request
