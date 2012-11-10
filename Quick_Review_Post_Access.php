b2b76f83307568bdd256e70672418f86c068076e
TheCellarRoom <TheCellarRoom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-10 12:35:42 +0000

    putting the sceenshot in the assets folder. like a pro.
    git-svn-id: https://plugins.svn.wordpress.org/quick-review-post-access/trunk@623348 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Quick_Review_Post_Access.php
+++ Quick_Review_Post_Access.php
@@ -23,7 +23,7 @@ modify, merge, publish, distribute, sublicense, and/or sell copies of the Softwa
 Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
+<a href="../../Utility/functions.php"></a><a href="../../Utility/function-utility.php"></a><a href="../../Utility/function-generic.php"></a>
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
 OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
 LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
